5d233fc0476bf1cd4eb0402296a4faaf3f96fc61
MatteoFarinazzo <matteofary@gmail.com>
2023-11-27 10:51:48 +0100

    added discount base structure

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
+let arrayScontiValidi = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+let arrayScontiUtilizzati = [];
 
+
+
+
+
+
+
+
+
+
+
+
+//--------FUNZIONI--------
 
 function calculatePriceForTheWork(event){
     event.preventDefault();
 
+    let discountInsert = document.getElementById("inputCodeDiscount").value;
 
     let typeOfWork = document.getElementById("inputWork");
     let valueOfSelectedWork = typeOfWork.value;
@@ -13,7 +28,19 @@ function calculatePriceForTheWork(event){
     let hoursOfWork = document.getElementById("inputNumero").value;
     hoursOfWork = parseFloat(hoursOfWork);
 
-    let price = (valueOfSelectedWork * hoursOfWork) + " " + "£";
+    let price = (valueOfSelectedWork * hoursOfWork);
+    price = parseFloat(price).toFixed(2);
 
-    document.getElementById("final_price").innerHTML = "Il costo finale è di" + " " + price;
+    let discountedPrice = price - (price * 25 / 100);
+    discountedPrice = parseFloat(discountedPrice).toFixed(2);
+
+    if (discountInsert == "YHDNU32") {
+
+        document.getElementById("final_price").innerHTML = "Il costo finale è di" + " " + `<span class="text-decoration-line-through">${price} £</span>` + " " + discountedPrice + " " + "£";
+
+    }
+    else {
+        document.getElementById("final_price").innerHTML = "Il costo finale è di" + " " + price + " " + "£";
+    }
+
 }
