4e9ea0b42aee60789e001eaf5fa6b3b84706261f
MatteoFarinazzo <matteofary@gmail.com>
2023-11-27 09:55:51 +0100

    started

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+function calculatePriceForTheWork(event){
+    event.preventDefault();
+
+
+let typeOfWork = document.getElementById("inputWork");
+let valueOfSelectedWork = typeOfWork.value;
+
+valueOfSelectedWork = parseFloat(valueOfSelectedWork);
+
+
+
+
+}
