8a1b5737a96290549e1c2a046f8f54e370ec0d7b
MatteoFarinazzo <matteofary@gmail.com>
2023-11-27 10:19:25 +0100

    base price calculator and HTML

--- js/main.js
+++ js/main.js
@@ -4,12 +4,16 @@ function calculatePriceForTheWork(event){
     event.preventDefault();
 
 
-let typeOfWork = document.getElementById("inputWork");
-let valueOfSelectedWork = typeOfWork.value;
+    let typeOfWork = document.getElementById("inputWork");
+    let valueOfSelectedWork = typeOfWork.value;
 
-valueOfSelectedWork = parseFloat(valueOfSelectedWork);
-
-
+    valueOfSelectedWork = parseFloat(valueOfSelectedWork);
 
 
+    let hoursOfWork = document.getElementById("inputNumero").value;
+    hoursOfWork = parseFloat(hoursOfWork);
+
+    let price = (valueOfSelectedWork * hoursOfWork) + " " + "£";
+
+    document.getElementById("final_price").innerHTML = "Il costo finale è di" + " " + price;
 }
