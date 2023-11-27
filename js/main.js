e93c35f923b7add688ffa33888510216f791f0dd
MatteoFarinazzo <matteofary@gmail.com>
2023-11-27 11:10:04 +0100

    discount function done

--- js/main.js
+++ js/main.js
@@ -34,10 +34,18 @@ function calculatePriceForTheWork(event){
     let discountedPrice = price - (price * 25 / 100);
     discountedPrice = parseFloat(discountedPrice).toFixed(2);
 
-    if (discountInsert == "YHDNU32") {
+    if (arrayScontiValidi.includes(discountInsert) && !arrayScontiUtilizzati.includes(discountInsert)) {
 
         document.getElementById("final_price").innerHTML = "Il costo finale è di" + " " + `<span class="text-decoration-line-through">${price} £</span>` + " " + discountedPrice + " " + "£";
-
+        arrayScontiUtilizzati.push(discountInsert);
+    } 
+    else if(arrayScontiValidi.includes(discountInsert) && arrayScontiUtilizzati.includes(discountInsert)){
+        alert("Hai già utilizzato questo codice!");
+        document.getElementById("final_price").innerHTML = "Il costo finale è di" + " " + price + " " + "£";
+    }
+    else if(!arrayScontiValidi.includes(discountInsert)){
+        alert("Il codice inserito non è valido");
+        document.getElementById("final_price").innerHTML = "Il costo finale è di" + " " + price + " " + "£";
     }
     else {
         document.getElementById("final_price").innerHTML = "Il costo finale è di" + " " + price + " " + "£";
